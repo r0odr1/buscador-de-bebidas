d3cafd297385f41d431abe588c71d49b7dd96355
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-06 16:10:47 -0500

    mostando el contenido de la receta en la modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,6 +6,7 @@ export default function Modal() {
 
   const modal = useAppStore((state) => state.modal)
   const closeModal = useAppStore((state) => state.closeModal)
+  const selectedRecipe = useAppStore((state) => state.selectedRecipe)
 
   return (
     <>
@@ -36,14 +37,23 @@ export default function Modal() {
               >
                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6" >
                   <Dialog.Title as="h3" className="text-gray-900 text-4xl font-extrabold my-5 text-center">
-                      Titulo Aquí
+                      {selectedRecipe.strDrink}
                   </Dialog.Title>
+
+                  <img
+                    src={selectedRecipe.strDrinkThumb}
+                    alt={`Imagen de ${selectedRecipe.strDrink}`}
+                    className='mx-auto w-96'
+                  />
+
                   <Dialog.Title as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                     Ingredientes y Cantidades
                   </Dialog.Title>
                   <Dialog.Title as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                     Instrucciones
                   </Dialog.Title>
+
+                  <p className='text-lg'>{selectedRecipe.strInstructions}</p>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
