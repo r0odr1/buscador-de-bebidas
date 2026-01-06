5f50c3d65b38df1f2a28f5b8eae2706bb9173f61
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-06 10:12:50 -0500

    Resaltando la pagina actual con NavLink

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 export default function Header() {
   return (
@@ -10,15 +10,15 @@ export default function Header() {
           </div>
 
           <nav className="flex gap-4">
-            <Link
+            <NavLink
               to="/"
-              className="text-white uppercase font-bold"
-            >Inicio</Link>
+              className={({isActive}) => isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'}
+            >Inicio</NavLink>
 
-            <Link
+            <NavLink
               to="/favoritos"
-              className="text-white uppercase font-bold"
-            >Favoritos</Link>
+              className={({isActive}) => isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'}
+            >Favoritos</NavLink>
           </nav>
 
 
