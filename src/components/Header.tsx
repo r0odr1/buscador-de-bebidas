826744bbfd0654eb2fc8d94e83219c9cde8f60b6
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-06 11:27:34 -0500

    Agregar theming en tailwind para anadir una imagen de fondo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
   console.log(isHome)
 
   return (
-    <header className="bg-slate-800">
+    <header className={ isHome ? 'bg-header bg-center bg-cover' : 'bg-slate-800' }>
       <div className="mx-auto container px-5 py-16">
         <div className="flex justify-between items-center">
           <div>
