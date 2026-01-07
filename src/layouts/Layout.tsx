6c72b9d98050ef5370b4454603cdc6e0da022d9b
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-07 10:51:16 -0500

    conectando las notificaciones con el slice

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header"
 import Modal from "../components/Modal"
 import { useEffect } from "react"
 import { useAppStore } from "../store/useAppStore"
+import Notification from "../components/Notification"
 
 export default function Layout() {
   
@@ -21,6 +22,7 @@ export default function Layout() {
       </main>
 
       <Modal />
+      <Notification />
     </>
   )
 }
