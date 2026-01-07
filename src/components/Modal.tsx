422ac245ccbdd0eaeaa629d9a3b2fa186922ccd2
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-07 09:17:56 -0500

    agregando botones para cerrar modal y agregar a favoritos

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -78,6 +78,24 @@ export default function Modal() {
                   </Dialog.Title>
 
                   <p className='text-lg'>{selectedRecipe.strInstructions}</p>
+
+                  <div className='mt-5 flex justify-between gap-4'>
+                    <button
+                      type='button'
+                      className='w-full rounded bg-gray-600 p-3 font-bold uppercase text-white shadow hover:bg-gray-500'
+                      onClick={closeModal}
+                    >
+                      Cerrar
+                    </button>
+
+                    <button
+                      type='button'
+                      className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500'
+                      onClick={() => handleClickFavorite()}
+                    >
+                      Agregar a Favoritos
+                    </button>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
