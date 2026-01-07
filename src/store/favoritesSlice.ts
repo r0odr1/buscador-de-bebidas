5110f1611ae31225236343ff38c871bad8305007
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-07 09:35:53 -0500

    Agregando los favoritos

--- src/store/favoritesSlice.ts
+++ src/store/favoritesSlice.ts
@@ -3,8 +3,20 @@ import type { Recipe } from '../types'
 
 export type FavoritesSliceType = {
   favorites: Recipe[]
+  handleClickFavorite: (recipe: Recipe) => void
 }
 
-export const createFavoritesSlice : StateCreator<FavoritesSliceType> = () => ({
-  favorites: []
+export const createFavoritesSlice : StateCreator<FavoritesSliceType> = (set, get) => ({
+  favorites: [],
+  handleClickFavorite: (recipe) => {
+    if(get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)) {
+      set((state) => ({
+        favorites: state.favorites.filter(favorite => favorite.idDrink !== recipe.idDrink)
+      }))
+    } else {
+      set((state) => ({
+        favorites: [ ...state.favorites, recipe]
+      }))
+    }
+  }
 })
