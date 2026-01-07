e8e52506aa264ce8919a9b53eb356057f4fd6792
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-07 12:32:03 -0500

    agregando slice de IA

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -3,9 +3,11 @@ import { devtools } from 'zustand/middleware'
 import { createRecipiesSlice, type RecipiesSliceType } from './recipeSlice'
 import { createFavoritesSlice, type FavoritesSliceType } from './favoritesSlice'
 import { createNotificationSlice, type NotificationSliceType } from './notificationSlice'
+import { createAISlice, type AISlice } from './aiSlice'
 
-export const useAppStore = create<RecipiesSliceType & FavoritesSliceType & NotificationSliceType>()(devtools( (...a) => ({
+export const useAppStore = create<RecipiesSliceType & FavoritesSliceType & NotificationSliceType & AISlice>()(devtools( (...a) => ({
   ...createRecipiesSlice(...a),
   ...createFavoritesSlice(...a),
-  ...createNotificationSlice(...a)
+  ...createNotificationSlice(...a),
+  ...createAISlice(...a)
 })))
