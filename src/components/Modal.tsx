8d40f181734ae0755e372d6816e58ab48baf1780
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-07 10:03:40 -0500

    agregar condicional del texto Agregar o eliminar favorito

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,6 +9,7 @@ export default function Modal() {
   const closeModal = useAppStore((state) => state.closeModal)
   const selectedRecipe = useAppStore((state) => state.selectedRecipe)
   const handleClickFavorite = useAppStore((state) => state.handleClickFavorite)
+  const favoriteExists = useAppStore((state) => state.favoriteExists)
 
   const renderIngredients = () => {
     const ingredients : JSX.Element[] = []
@@ -94,7 +95,7 @@ export default function Modal() {
                       className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500'
                       onClick={() => handleClickFavorite(selectedRecipe)}
                     >
-                      Agregar a Favoritos
+                      {favoriteExists(selectedRecipe.idDrink) ? 'Eliminar Favorito' : 'Agregar Favorito'}
                     </button>
                   </div>
                 </Dialog.Panel>
